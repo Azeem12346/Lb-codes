c7a1ce245442ce8f93cc4c24cbc1a7eb8d81c9aa
Azeem <azshk31904@gmail.com>
2026-06-11 11:26:17 +0530

    daily updaye

---
+++ Desktop/git/q2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+        
+        System.out.println("number\tsquare\tcube");
+        
+        
+        for( int i=1;i<=5;i++){
+           int  sq = i*i;
+           int cube = i*i*i; 
+           System.out.println(i+ "\t" + sq + "\t" + cube );
+    }
+     
+
+    
+}}
